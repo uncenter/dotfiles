a9ecf59d9fdfb5c7422cb01d3004d15dc913f508
uncenter <47499684+uncenter@users.noreply.github.com>
2023-07-16 09:09:43 +0800

    refactor(update): comments, Fnmfile

--- fnm.sh
+++
@@ -1,2 +0,0 @@
-fnm install v18.16.0 && fnm alias v18.16.0 lts
-fnm install v20.4.0 && fnm alias v20.4.0 default
--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 # 
 # update.sh - update dotfiles
 
+# Ensure required commands are availables
 cmds=(brew fnm npm pip3 git)
 for cmd in ${cmds[@]}; do
     if ! command -v $cmd &> /dev/null; then
@@ -12,6 +13,8 @@ done
 
 echo "Updating Brewfile..."
 brew bundle dump --force
+
+# Save global npm packages for each node installation
 node_versions=$(fnm list | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+')
 node_current=$(fnm current)
 for v in $node_versions; do
@@ -20,8 +23,14 @@ for v in $node_versions; do
     npm list --global --parseable --depth=0 | sed '1d' |awk -F"/node_modules/" '{print $2}' > Npmfile-$v
 done
 fnm use $node_current &> /dev/null
+
+# Save fnm versions and aliases
+echo "Updating Fnmfile..."
+fnm list | grep -Eo '(v[0-9]+\.[0-9]+\.[0-9]+)( [A-Za-z0-9_]+)?' | awk '{print $1, $2}' > Fnmfile
+
 echo "Updating requirements.txt..."
 pip3 freeze --user > requirements.txt
+
 read -r -n 1 -p "Commit and push? [y/N] " response
 echo
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
