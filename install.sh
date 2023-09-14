9e0032e71931d68394ef2a61eef7f66c26033f70
uncenter <47499684+uncenter@users.noreply.github.com>
2023-09-13 21:11:57 -0400

    chore: update dotfiles

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ else
     brew update
 fi
 brew upgrade
-brew install gum stow
+brew install stow
 
 if ! have "git"; then
     brew install git
@@ -29,32 +29,30 @@ git clone $SOURCE $DOTFILES
 
 echo "Stowing dotfiles..."
 for dir in */$DOTFILES; do
-    gum spin --title "Stowing $DOTFILES/$dir to $HOME/$dir..." -- stow --target="$HOME" "$DOTFILES/$dir"
+    echo "Stowing $DOTFILES/$dir to $HOME/$dir..."
+    stow --target="$HOME" "$DOTFILES/$dir"
 done
 
 for f in ~/.bin/*; do chmod +x $f; done
 
-gum spin --title "Installing Homebrew packages..." -- brew bundle install --file=Brewfile
+echo "Installing Homebrew packages..."
+brew bundle install --file=Brewfile
 brew cleanup --prune=all
-
-fnmfile() {
-    cat 'Fnmfile' | while read -r line; do
-        version=$(echo $line | awk '{print $1}')
-        alias=$(echo $line | awk '{print $2}')
-        if [ -z $alias ]; then
-            alias=""
-        fi
-        if ! fnm list | grep -Eo $version &> /dev/null; then
-            fnm install $version
-        fi
-        fnm alias $version $alias
-    done
-}
-gum spin --title "Installing and aliasing Node.js versions..." -- fnmfile
-pnpmfile() {
-    cat 'Pnpmfile' | while read -r line; do
-        pnpm install --global $line
-    done
-}
-gum spin --title "Installing global npm packages..." -- pnpmfile
-gum spin --title "Installing global Python packages..." -- pip3 install --user -r requirements.txt
+echo "Installing and aliasing Node.js versions..."
+cat 'Fnmfile' | while read -r line; do
+    version=$(echo $line | awk '{print $1}')
+    alias=$(echo $line | awk '{print $2}')
+    if [ -z $alias ]; then
+        alias=""
+    fi
+    if ! fnm list | grep -Eo $version &> /dev/null; then
+        fnm install $version
+    fi
+    fnm alias $version $alias
+done
+echo "Installing global npm packages..."
+cat 'Pnpmfile' | while read -r line; do
+    pnpm install --global $line
+done
+echo "Installing global Python packages..."
+pip3 install --user -r requirements.txt
