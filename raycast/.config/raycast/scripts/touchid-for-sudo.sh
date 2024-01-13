c62a936f3d87e1892f5f5f758671585258f327c7
uncenter <47499684+uncenter@users.noreply.github.com>
2024-01-12 18:17:57 -0700

    feat(raycast): init scripts

---
+++ raycast/.config/raycast/scripts/touchid-for-sudo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+// @raycast.schemaVersion 1
+// @raycast.title Enable TouchID for `sudo`
+// @raycast.mode compact
+
+// @raycast.icon 👆🏼
+
+// @raycast.description Runs a script to enable/re-enable TouchID for the `sudo` command
+// @raycast.author uncenter
+// @raycast.authorURL https://github.com/uncenter
+
+
+curl -fsSL https://gist.githubusercontent.com/uncenter/604233698c41f1c54079760b93fbabf0/raw/x.sh | sh
