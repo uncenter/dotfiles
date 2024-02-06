47f03a24fef74929001df14c9e2e6bace256ebd8
uncenter <47499684+uncenter@users.noreply.github.com>
2024-02-06 16:54:56 -0500

    feat(nvim): auto-rename html tags

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,20 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		version = "*",
 		build = ":TSUpdate",
+		dependencies = {
+			{
+				"windwp/nvim-ts-autotag",
+				main = "nvim-ts-autotag",
+				opts = {},
+			},
+		},
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
 				auto_install = true,
+				auto_tag = { enable = true },
 				highlight = { enable = true },
 				indent = { enable = true },
 			})
