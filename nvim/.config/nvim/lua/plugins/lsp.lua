972682c6b5b1e8f7ae9a2c533039b5488d315078
uncenter <47499684+uncenter@users.noreply.github.com>
2024-03-01 09:38:35 -0500

    refactor(nvim): remove format on save

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -27,7 +27,6 @@ return {
 		"nvimtools/none-ls.nvim",
 		config = function()
 			local null_ls = require("null-ls")
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
@@ -44,19 +43,6 @@ return {
 					null_ls.builtins.diagnostics.eslint,
 					null_ls.builtins.diagnostics.shellcheck,
 				},
-				-- https://github.com/nvimtools/none-ls.nvim/wiki/Formatting-on-save
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({ async = false })
-							end,
-						})
-					end
-				end,
 			})
 		end,
 	},
