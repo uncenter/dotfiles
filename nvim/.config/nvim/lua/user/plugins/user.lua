fc28b6a7a907c1acf388db8d8bf89287ddb9bef3
uncenter <47499684+uncenter@users.noreply.github.com>
2024-03-06 06:02:43 -0500

    feat(nvim): add nvim-silicon

--- nvim/.config/nvim/lua/user/plugins/user.lua
+++ nvim/.config/nvim/lua/user/plugins/user.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    opts = {
+      disable_defaults = true,
+      language = function() return vim.bo.filetype end,
+      output = function() return "./" .. os.date "!%Y-%m-%dT%H-%M-%S" .. "_code.png" end,
+    },
+  },
+}
