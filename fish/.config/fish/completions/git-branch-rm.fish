4fd618cc625a3aa33d08f5ff2414ac1b77eec4d6
uncenter <47499684+uncenter@users.noreply.github.com>
2024-04-11 20:14:44 -0400

    chore: cleanup, fixes, misc

--- fish/.config/fish/completions/git-branch-rm.fish
+++ fish/.config/fish/completions/git-branch-rm.fish
@@ -1,6 +1,6 @@
 function __fish_git_branch_rm
   if git rev-parse --is-inside-work-tree &>/dev/null
-    complete -c git-branch-rm --no-files -a "(git branch | cut -c 3- | awk '{print $2}')"
+    complete -c git-branch-rm --no-files -a "(git branch | cut -c 3-)"
   end
 end
 
